f6f7c00f79e1be5e68edb92df00b10d08e01c449
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2025-10-08 21:35:09 -0400

    Fixed issues

--- SportyGo/app.config.js
+++ SportyGo/app.config.js
@@ -11,7 +11,7 @@ const getUniqueIdentifier = () => {
       return 'com.sparkpro.sportygo.preview';
     }
   
-    return 'com.sparkpro.SportyGo';
+    return 'com.sparkpro.sportygo';
   };
   
   const getAppName = () => {
