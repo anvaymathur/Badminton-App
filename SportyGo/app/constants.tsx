58501a3e38779dc3a4f02543b6f685168ac86dd0
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2025-11-05 20:54:23 -0500

    Added constants to git commit

---
+++ SportyGo/app/constants.tsx
@@ -0,0 +1,12 @@
+export default class constants {
+
+    //Auth0 Domain
+    static readonly Auth0Domain = 'sportygo.ca.auth0.com';
+    //Auth0 ClientId
+    static readonly Auth0ClientId = 'TzevmHUuWdBTRK9jmAkswj4bBAUyvovB';
+
+    //Pendo APIKey
+    static readonly PendoAPIKey = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhY2VudGVyIjoidXMiLCJrZXkiOiJmNmE0N2I0YzY5NDM1OGE1ZjUyN2UzOGQwNDliNzg3NjYxYzk2ZDYzY2QzZThmY2ZmOTA5NzM3MjM4MGU5YmZjZDE1ZDVkYjA5Yjg3M2RmZjU1ZjgwNjM3ZDM3ZDFhMGU2MGUwYWQ2ZmM0ZGUwNjE0YTA1ZmJjZGZhMzU4YmZlNjQ0NjUxYjRkOTg0MmUwMWI4M2YwNTMzY2JiMmVkMzk0ZjgwOGEwM2ZjNDYxNjNjOWE2YWEwMDI2MTJhYjkxYzIwYTY5MzAwZWExZjU2N2U1NDguODgyY2RjODMzMzc4OWU2ZWE2Mjc0YzEwNGFkYmJhMjEuODA0MTEyNDRmMzJjY2RjZTM0ZTk4YjI1YzVkODllNzIwMGM1ZDUxNWNhOGU4NjJiNTQxNjg4NzNkMGQ4NmFjMiJ9.RtvtdTC8rJgPkMh4YCuF7_UpFJKkL1GyCXOVrBNNwKov3t-n6TVN37qzGMlHK9JN01hPW5uKxaySN9cY2ashDeE6yV9Dl9WQyZnAjPLuHfS0b277hT5Sr3NAoQIYCxcHFQf8F7KK5C-e8zh3LZjXMAs3Jum8yNT__oJuxBMQNJE';
+    //Pendo Scheme
+    static readonly PendoScheme = 'pendo-81693a93';
+}
